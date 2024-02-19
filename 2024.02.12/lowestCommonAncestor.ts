4909e05c6bc609a23a73a2983b939376cc6097e9
이승표 <zlmmxe@naver.com>
2024-02-19 09:47:01 +0900

    Create lowestCommonAncestor.ts

---
+++ 2024.02.12/lowestCommonAncestor.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+	const bothSmaller = p.val < root.val && q.val < root.val;
+    const bothBigger = p.val > root.val && q.val > root.val;
+
+    if (!bothSmaller && !bothBigger) {
+        return root;
+    }
+
+    if (bothSmaller) {
+        return lowestCommonAncestor(root.left, p, q);
+    }
+
+    if (bothBigger) {
+        return lowestCommonAncestor(root.right, p, q);
+    }
+};
